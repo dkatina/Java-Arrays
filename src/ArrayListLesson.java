c333721ab32597bb622922f0c4956a62d2361365
dkatina <108095842+dkatina@users.noreply.github.com>
2025-02-19 12:57:08 -0500

    array lists

---
+++ src/ArrayListLesson.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class ArrayListLesson {
+
+
+    public static void main(String[] args) {
+
+        //ArrayLists are a dynamically sized class that work more similarly to JS and Python arrays.
+        ArrayList<String> myList = new ArrayList<>();
+        //Creates a new arraylist of strings
+
+
+        //add elements
+        myList.add("apple");
+        myList.add("banana");
+        System.out.println((myList));
+    }
+}
