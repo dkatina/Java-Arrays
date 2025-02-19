b3bc4084f2b0e3c0a7ce63bd45b1a1c3dae8e476
dkatina <108095842+dkatina@users.noreply.github.com>
2025-02-19 12:30:42 -0500

    sorting arrays

--- src/ArrayTypes.java
+++ src/ArrayTypes.java
@@ -12,7 +12,7 @@ public class ArrayTypes {
         }
         System.out.println(Arrays.toString(nums));
 
-        //All of the following are valid declarations of an array
+        //All the following are valid declarations of an array
         //int[] nums
         //String [] strs
         //boolean bools[]
@@ -21,5 +21,26 @@ public class ArrayTypes {
         //can also define arrays with initial values
         int[] nums2 = {1,2,3,4,5};
         System.out.println(Arrays.toString(nums2));
+
+        //accessing last index (length of array - 1)
+        System.out.println(nums2[nums2.length - 1]);
+
+        //sorting arrays with Arrays.sort()
+        int[] num3 = {4,5,1,67,2,4,1};
+        System.out.println(Arrays.toString(num3));
+        Arrays.sort(num3);
+        System.out.println(Arrays.toString(num3));
+
+        //sorting words in Alphabetic order
+        String[] words = {"banana", "apple", "cherry", "date"};
+
+// Print the array before sorting
+        System.out.println("Before sorting: " + Arrays.toString(words));
+
+// Sort the array
+        Arrays.sort(words);
+
+// Print the array after sorting
+        System.out.println("After sorting: " + Arrays.toString(words));
     }
 }
