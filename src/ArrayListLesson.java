3db873937336858f26b958eb23e826b3834a976c
dkatina <108095842+dkatina@users.noreply.github.com>
2025-02-27 12:35:22 -0500

    arraylist methods

--- src/ArrayListLesson.java
+++ src/ArrayListLesson.java
@@ -14,5 +14,39 @@ public class ArrayListLesson {
         myList.add("apple");
         myList.add("banana");
         System.out.println((myList));
+
+        //Arraylist Methods
+
+        //.set(index,replacement)
+        myList.set(1,"blueberry");
+        System.out.println(myList);
+
+        //,isEmpty() returns bool T/F if list is empty
+        System.out.println(myList.isEmpty());
+
+        //.size() == len()
+        System.out.println(myList.size());
+
+        // .contains(element) returns T/F depeneding on if item in array
+        System.out.println(myList.contains("blueberry"));
+
+        //.clear() remove all elements form list
+
+        ArrayList<String> list2 = new ArrayList<>();
+        list2.add("apple");
+        list2.add("banana");
+        System.out.println((myList));
+        list2.set(1,"blueberry");
+
+        //Checks if two lists have same contents in same order
+        System.out.println(myList.equals(list2));
+
+        // .remove(index)
+        list2.remove(0);
+        System.out.println(list2);
+
+        // .remove(element)
+        list2.remove("blueberry");
+        System.out.println(list2);
     }
 }
